e8b0268d248d0dbcff251fe1105a6878ab496565
Mike Thomson <michaelgt04@gmail.com>
2017-03-16 13:52:40 -0400

    Added setup for react-foundations app

---
+++ app/data.js
@@ -0,0 +1,19 @@
+const data = {
+  foods: [
+    {
+      name: "Double Chocolate",
+      delicious: true,
+      imgUrl: "http://www.yourchampionshipcompany.com/wp-content/uploads/2014/01/DD_DoubleChocolate.jpg"
+    },
+    {
+      name: "Chocolate Frosted",
+      delicious: false,
+      imgUrl: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Dunkin-Donuts-Chocolate-Glazed.jpg"
+    },
+    {
+      name: "Chocolate Frosted with Sprinkles",
+      delicious: true,
+      imgUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/24/13/enhanced/webdr05/enhanced-buzz-1864-1424801781-9.jpg"
+    }
+  ]
+}
